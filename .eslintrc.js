9bce5d4d8b89d6966b71d16118daef4ac6426f03
TinsFox <fox@tinsfox.com>
2022-02-20 00:04:32 +0800

    chore: update specification restrictions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: [
-    "eslint-config-ali/typescript/react",
-    "plugin:prettier/recommended"
-  ],
-  globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
-    page: true,
-  },
-  plugins: ['prettier'],
-  rules: {
-    'import/no-cycle': 'off',
-    'react/prop-types': 0,
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    'prettier/prettier': 'error',
-  },
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
 };
