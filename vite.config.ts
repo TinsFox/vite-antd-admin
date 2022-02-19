baefaca4b914d5aa69e5030cef235a9d8a59a09f
Luoboz <847267507@qq.com>
2022-02-20 00:19:14 +0800

    feat: router manager

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+    },
+  },
   server: {
     host: "0.0.0.0",
     port: 4000,
