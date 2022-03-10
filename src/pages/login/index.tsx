866546175cbd32eaa1625aed93783bbbd9e00635
TinsFox <fox@tinsfox.com>
2022-03-10 11:21:50 +0800

    feat: login page done

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,63 +1,148 @@
-import { Button, Form, Input } from 'antd';
+import {
+  AlipayCircleOutlined,
+  LockOutlined,
+  MobileOutlined,
+  TaobaoCircleOutlined,
+  UserOutlined,
+  WeiboCircleOutlined,
+} from '@ant-design/icons';
+import { LoginForm, ProFormCaptcha, ProFormCheckbox, ProFormText } from '@ant-design/pro-form';
+import { message, Space, Tabs } from 'antd';
+import type { CSSProperties } from 'react';
 import React, { useState } from 'react';
 import styles from './index.module.less';
-import logo from '/static/images/logo.svg';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+type LoginType = 'phone' | 'account';
 
-/* 登陆表单 */
-interface ILogin {
-  email: string;
-  password: string;
-}
-
-const Login: React.FC = () => {
-  const [form, setForm] = useState<ILogin>();
-  const onSubmitForm = (values: ILogin) => {
-    setForm(values);
-    console.log('Received values of form: ', values);
-  };
-  const renderLabel = (label: string) => <div className={styles.label}>{label}</div>;
+const iconStyles: CSSProperties = {
+  marginLeft: '16px',
+  color: 'rgba(0, 0, 0, 0.2)',
+  fontSize: '24px',
+  verticalAlign: 'middle',
+  cursor: 'pointer',
+};
+export default function Login() {
+  const [loginType, setLoginType] = useState<LoginType>('account');
   return (
-    <div className={styles.login}>
-      <div className={styles['login-box']}>
+    <div className={styles.container}>
+      <div className={styles.loginBox}>
+        <div>Lottery Dashboard</div>
+        <div>Login in to Lottery Dashboard</div>
         <div>
-          <img src={logo} className={styles.logo} />
-          <div className={styles.title}>Vite Antd Admin</div>
-          <div className={styles.tip}>Login in to Vite Antd Admin</div>
-        </div>
-        <div>
-          <Form
-            name="normal_login"
-            initialValues={form}
-            className={styles['login-form']}
-            onFinish={onSubmitForm}
-            layout="vertical"
+          <LoginForm
+            logo="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+            title="Github"
+            subTitle="全球最大同性交友网站"
+            actions={
+              <Space>
+                其他登录方式
+                <AlipayCircleOutlined style={iconStyles} />
+                <TaobaoCircleOutlined style={iconStyles} />
+                <WeiboCircleOutlined style={iconStyles} />
+              </Space>
+            }
           >
-            <Form.Item
-              label={renderLabel('EMAIL')}
-              name="email"
-              style={{ marginBottom: '10px' }}
-              rules={[{ message: 'Please input your Email!' }]}
+            <Tabs
+              activeKey={loginType}
+              onChange={(activeKey) => setLoginType(activeKey as LoginType)}
             >
-              <Input prefix={<UserOutlined />} placeholder="Email address" />
-            </Form.Item>
-            <Form.Item
-              label={renderLabel('PASSWORD')}
-              name="password"
-              rules={[{ message: 'Please input your Password!' }]}
+              <Tabs.TabPane key={'account'} tab={'账号密码登录'} />
+              <Tabs.TabPane key={'phone'} tab={'手机号登录'} />
+            </Tabs>
+            {loginType === 'account' && (
+              <>
+                <ProFormText
+                  name="username"
+                  fieldProps={{
+                    size: 'large',
+                    prefix: <UserOutlined className={'prefixIcon'} />,
+                  }}
+                  placeholder={'用户名: admin or user'}
+                  rules={[
+                    {
+                      required: true,
+                      message: '请输入用户名!',
+                    },
+                  ]}
+                />
+                <ProFormText.Password
+                  name="password"
+                  fieldProps={{
+                    size: 'large',
+                    prefix: <LockOutlined className={'prefixIcon'} />,
+                  }}
+                  placeholder={'密码: ant.design'}
+                  rules={[
+                    {
+                      required: true,
+                      message: '请输入密码！',
+                    },
+                  ]}
+                />
+              </>
+            )}
+            {loginType === 'phone' && (
+              <>
+                <ProFormText
+                  fieldProps={{
+                    size: 'large',
+                    prefix: <MobileOutlined className={'prefixIcon'} />,
+                  }}
+                  name="mobile"
+                  placeholder={'手机号'}
+                  rules={[
+                    {
+                      required: true,
+                      message: '请输入手机号！',
+                    },
+                    {
+                      pattern: /^1\d{10}$/,
+                      message: '手机号格式错误！',
+                    },
+                  ]}
+                />
+                <ProFormCaptcha
+                  fieldProps={{
+                    size: 'large',
+                    prefix: <LockOutlined className={'prefixIcon'} />,
+                  }}
+                  captchaProps={{
+                    size: 'large',
+                  }}
+                  placeholder={'请输入验证码'}
+                  captchaTextRender={(timing, count) => {
+                    if (timing) {
+                      return `${count} ${'获取验证码'}`;
+                    }
+                    return '获取验证码';
+                  }}
+                  name="captcha"
+                  rules={[
+                    {
+                      required: true,
+                      message: '请输入验证码！',
+                    },
+                  ]}
+                  onGetCaptcha={async () => {
+                    message.success('获取验证码成功！验证码为：1234');
+                  }}
+                />
+              </>
+            )}
+            <div
+              style={{
+                marginBottom: 24,
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
             >
-              <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className={styles['login-form-button']}>
-                Log in
-              </Button>
-            </Form.Item>
-          </Form>
+              <ProFormCheckbox noStyle name="autoLogin">
+                自动登录
+              </ProFormCheckbox>
+              <a>忘记密码</a>
+            </div>
+          </LoginForm>
         </div>
       </div>
     </div>
   );
-};
-
-export default Login;
+}
